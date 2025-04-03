1f3b2607075b6d6efaea1f778dfc732f427c3794
Vofchuk <daniel@vizo.dev>
2025-04-03 10:45:52 -0300

    Add Portuguese translations for SearchUserStatus enum
    - Introduce a `toPortuguese` getter in the SearchUserStatus enum to provide Portuguese translations for each status.
    - Enhance the user experience by supporting multilingual status representation in the application.

--- lib/models/enums/incident_search_user_status.dart
+++ lib/models/enums/incident_search_user_status.dart
@@ -7,4 +7,26 @@ enum SearchUserStatus {
   ADMIN_VIEW,
   EXPIRED,
   ACCEPTED_WITH_NO_ASSIGNMENT;
+
+  String get toPortuguese {
+    switch (this) {
+      case SearchUserStatus.IN_LINE:
+        return 'Na fila';
+
+      case SearchUserStatus.REQUESTED:
+        return 'Solicitado';
+      case SearchUserStatus.ACCEPTED:
+        return 'Aceito';
+      case SearchUserStatus.ABORTED:
+        return 'Cancelado';
+      case SearchUserStatus.REJECTED:
+        return 'Rejeitado';
+      case SearchUserStatus.ADMIN_VIEW:
+        return 'Visualização administrativa';
+      case SearchUserStatus.EXPIRED:
+        return 'Expirado';
+      case SearchUserStatus.ACCEPTED_WITH_NO_ASSIGNMENT:
+        return 'Aceito sem vagas';
+    }
+  }
 }
